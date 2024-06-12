cf5ee7282673a5e07be00af86c5e5cde256e2f3b
HenryHuang <henry.2039@gmail.com>
2024-06-12 18:37:33 +0800

    fix text

--- src/components/NvaBarContainer.tsx
+++ src/components/NvaBarContainer.tsx
@@ -20,10 +20,10 @@ export default observer(function NvaBarContainer() {
                     <a href="/react-game/" className="nav-link">Home</a>
                 </li>
                 <li className="nav-item">
-                    <a href="/react-game/rotate" className="nav-link">Rotate 旋轉棋</a>
+                    <a href="/react-game/rotate" className="nav-link">旋轉棋</a>
                 </li>
                 <li className="nav-item">
-                    <a href="/react-game/5seconds" className="nav-link">5 seconds 反應力</a>
+                    <a href="/react-game/5seconds" className="nav-link">5秒反應</a>
                 </li>
             </ul>
         </nav>
